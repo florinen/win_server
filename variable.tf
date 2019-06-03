eb11c98597a27c046c9d080105eae5e445c9277f
florinen <florinen>
2019-06-03 10:18:10 -0500

    updated key path

--- variable.tf
+++ variable.tf
@@ -4,7 +4,7 @@ variable "AWS_REGION" { default = "eu-west-1" }
 variable "VPC_ID" {}
 variable "AZ" {}
 variable "WIN_AMIS" { }  
-variable "PATH_TO_PRIVATE_KEY" { default = "~/.ssh/id_rsa" } 
-variable "PATH_TO_PUBLIC_KEY" { default = "~/.ssh/id_rsa.pub" } 
+variable "PATH_TO_PRIVATE_KEY" { default = "/home/fnenciu/windows/win_key" } 
+variable "PATH_TO_PUBLIC_KEY" { default = "/home/fnenciu/windows/win_key.pub" } 
 variable "INSTANCE_USERNAME" { default = "admin" } 
 variable "INSTANCE_PASSWORD" { }
