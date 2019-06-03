c16048e6e825befd704f113bb739ebbdd2c36c7f
florinen <nenfl24@gmail.com>
2019-06-02 19:52:16 -0500

    aded var subnet

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
-variable "AWS_REGION" { 
-default = "eu-west-1" 
-}
+variable "AWS_REGION" { default = "eu-west-1" }
+variable "VPC_ID" {}
+variable "SUBNET_ID" {}
 variable "WIN_AMIS" { }  
 variable "PATH_TO_PRIVATE_KEY" { default = "~/.ssh/id_rsa" } 
 variable "PATH_TO_PUBLIC_KEY" { default = "~/.ssh/id_rsa.pub" } 
