f721e1d1defb9a73921cb290dca75e5b85143f50
florinen <florinen>
2019-06-03 14:31:59 -0500

    added new var

--- variable.tf
+++ variable.tf
@@ -8,3 +8,4 @@ variable "PATH_TO_PRIVATE_KEY" { default = "/home/fnenciu/windows/win_key" }
 variable "PATH_TO_PUBLIC_KEY" { default = "/home/fnenciu/windows/win_key.pub" } 
 variable "INSTANCE_USERNAME" { default = "Administrator" } 
 variable "INSTANCE_PASSWORD" { }
+variable "INSTANCE_TYPE" {}
