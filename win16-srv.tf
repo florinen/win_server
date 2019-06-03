4f8156e83ac91ff5217cf9b59b0ac2b4e6e46dc7
florinen <florinen>
2019-06-03 14:53:29 -0500

    updated

--- win16-srv.tf
+++ win16-srv.tf
@@ -8,12 +8,28 @@ resource "aws_instance" "win16-srv" {
     instance_type = "${var.INSTANCE_TYPE}"
     key_name = "${aws_key_pair.win_key.key_name}"
     availability_zone = "${var.AZ}"
-    user_data = "${var.USER_DATA}"
-    
+    user_data = <<EOF
+<powershell>
+net user ${var.INSTANCE_USERNAME} ‘${var.INSTANCE_PASSWORD}’ /add /y
+net localgroup administrators ${var.INSTANCE_USERNAME} /add
+
+winrm quickconfig -q
+winrm set winrm/config/winrs ‘@{MaxMemoryPerShellMB=”300″}’
+winrm set winrm/config ‘@{MaxTimeoutms=”1800000″}’
+winrm set winrm/config/service ‘@{AllowUnencrypted=”true”}’
+winrm set winrm/config/service/auth ‘@{Basic=”true”}’
+
+netsh advfirewall firewall add rule name=”WinRM 5985″ protocol=TCP dir=in localport=5985 action=allow
+netsh advfirewall firewall add rule name=”WinRM 5986″ protocol=TCP dir=in localport=5986 action=allow
+
+net stop winrm
+sc.exe config winrm start=auto
+net start winrm
+</powershell>
+EOF
     tags = {
       Name = "Win16-SRV"
     }
-
 
 # The connection block tells our provisioner how to
   # communicate with the resource (instance)
