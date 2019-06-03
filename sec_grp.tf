60924c79bcbb8e00689021b65bfc60be858bc8d3
florinen <florinen>
2019-06-03 15:22:08 -0500

    fixed

--- sec_grp.tf
+++ sec_grp.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "Allow-RDP" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from = 22
+    from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
