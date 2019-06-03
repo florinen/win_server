eadd82d8990f1124579d7447e20a70725d983172
florinen <nenfl24@gmail.com>
2019-06-02 21:46:56 -0500

    updated

--- win16-srv.tf
+++ win16-srv.tf
@@ -1,7 +1,4 @@
-resource "aws_key_pair" "my_key" {
-    key_name = "my_key"
-    public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-}
+
 
 resource "aws_instance" "win16-srv" {
     ami = "${var.WIN_AMIS}"
