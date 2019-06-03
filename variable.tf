611f4c523cb4c869ca9544c3a020acdb3eecd9e1
florinen <nenfl24@gmail.com>
2019-06-02 19:38:22 -0500

    added path to ssh

--- variable.tf
+++ variable.tf
@@ -4,7 +4,7 @@ variable "AWS_REGION" {
 default = "eu-west-1" 
 }
 variable "WIN_AMIS" { }  
-variable "PATH_TO_PRIVATE_KEY" { default = "id_rsa" } 
-variable "PATH_TO_PUBLIC_KEY" { default = "id_rsa.pub" } 
+variable "PATH_TO_PRIVATE_KEY" { default = "~/.ssh/id_rsa" } 
+variable "PATH_TO_PUBLIC_KEY" { default = "~/.ssh/id_rsa.pub" } 
 variable "INSTANCE_USERNAME" { default = "admin" } 
 variable "INSTANCE_PASSWORD" { }
