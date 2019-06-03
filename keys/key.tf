386a4cf968c859d270a7567c29a16d6c3ec2286b
florinen <florinen>
2019-06-03 09:59:04 -0500

    updated keys path

--- keys/key.tf
+++ keys/key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "my_key" {
     key_name = "my_key"
-    public_key = "${file("/home/fnenciu/windows/win_key.pub")}"
+    public_key = "${file("/root/acirrustech.com/windows/config/my_key.pub")}"
 }
