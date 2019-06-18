2c0daff3d67e1f90ea7c37c6e0e18b90a08ada0b
florinen <nenfl24@gmail.com>
2019-06-18 09:49:15 -0500

    allow rdp added

--- sec_grp.tf
+++ sec_grp.tf
@@ -1,19 +1,23 @@
-resource "aws_security_group" "allow-all" {
-    name    ="allow-all"
+resource "aws_security_group" "allowed-ports" {
+    name    ="allowed-ports"
     vpc_id  = "${var.VPC_ID}"
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Everything allowed out"
   }
+  
   ingress {
-    from_port   = 0
-    to_port     = 6556
+    from_port   = 3389
+    to_port     = 3389
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow RDP connections"
   }
+
   tags = {
-    name = "Allow-RDP"
+    name = "Allowed-Ports"
   }
 }
