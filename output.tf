285ed6c86afd6a2ae5c44f54bbe65a2f19960a8f
florinen <nenfl24@gmail.com>
2019-06-19 15:16:24 -0500

    updated id

--- output.tf
+++ output.tf
@@ -2,16 +2,18 @@ output "instance_ips" {
     description = "Windoes Active Directory publiuc IP"
   value = ["${aws_instance.win16-srv.*.public_ip}"]
 }
-output "public_dns" {
-  value = "${locat.public_dns}"
-}
-output "ip" {
-    description = "EIP"
-  value = "${aws_eip.ip.public_ip}"
+#output "public_dns" {
+#  value = "${locat.public_dns}"
+#}
+#output "ip" {
+#    description = "EIP"
+#  value = "${aws_eip.ip.public_ip}"
 }
 
-output "id" {
-  value = "${join("",aws_instance.default.*.id)}"
+output "server_id" {
+    description = "Win16-srv id"
+  value = "${aws_instance.win16-srv.*.id}"
 }
+
 
 
