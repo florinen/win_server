5fa6ab619c6b476352e717b37cd9bdbb51ce0fa0
florinen <nenfl24@gmail.com>
2019-06-19 12:08:26 -0500

    removed restart

--- ad/ad_init.ps1
+++ ad/ad_init.ps1
@@ -11,7 +11,9 @@ $LogPath = "C:\Windows\NTDS"
 $SysvolPath = "C:\Windows\SYSVOL"
 
 ## Configures script to run once on next logon
+Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'AD_Create' -Value "c:\windows\system32\cmd.exe /c C:\scripts\ad_init.bat"
 Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'AD-Step2' -Value "c:\windows\system32\cmd.exe /c C:\scripts\ad_add_domain_users.bat"
+
 
 # Registry path for Autologon configuration
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
