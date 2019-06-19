5e0001977fb8a3c06e09712b7253baca5771b646
florinen <nenfl24@gmail.com>
2019-06-19 13:33:21 -0500

    some updates

--- ad/ad_add_domain_users.ps1
+++ ad/ad_add_domain_users.ps1
@@ -5,7 +5,7 @@ $DomainEnding = "com"
 Import-Module ActiveDirectory
 
 New-ADOrganizationalUnit -path "dc=$Domain, dc=$DomainEnding" -name $OrganizationalUnit -ProtectedFromAccidentalDeletion:$false
-New-ADUser -Path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" -Name "Admin" -AccountPassword (ConvertTo-SecureString "Summer01!" -AsPlaintext -Force) -Description "Acirrustech Setup Account" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -PassThru | Enable-ADAccount
+New-ADUser -Path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" -Name "Admin" -AccountPassword (ConvertTo-SecureString "test123-Admin!!!8" -AsPlaintext -Force) -Description "Acirrustech Setup Account" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -PassThru | Enable-ADAccount
 #New-ADUser -Path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" -Name "Test" -AccountPassword (ConvertTo-SecureString "Summer01!" -AsPlaintext -Force) -Description "Acirrustech test user" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -PassThru | Enable-ADAccount
 
 Add-ADGroupMember 'Domain Admins' Admin
