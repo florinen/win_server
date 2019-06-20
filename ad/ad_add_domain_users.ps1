337ed00cd6058d7922f8a354ab38a99624a2d9a0
florinen <nenfl24@gmail.com>
2019-06-20 15:50:58 -0500

    changed user

--- ad/ad_add_domain_users.ps1
+++ ad/ad_add_domain_users.ps1
@@ -4,7 +4,7 @@ $Domain = "acirrustech"
 $DomainEnding = "com"
 $City = "Chicago"
 $Group = "Administrators"
-$Groups = 'Domain Admins','Domain Users','Administrators','Enterprise Admins','Group Policy Creator Owners','Schema Admins'
+$Groups = @('Domain Admins','Domain Users','Administrators','Enterprise Admins','Group Policy Creator Owners','Schema Admins')
 $User = "Admin"
 $AccountPassword = "Summer-01!!!*"
 # Configures script to run once on next logon
@@ -13,17 +13,17 @@ $AccountPassword = "Summer-01!!!*"
 Import-Module ActiveDirectory
 New-ADOrganizationalUnit -Name $OrganizationalUnit -City $City -path "dc=$Domain,dc=$DomainEnding"
 New-ADOrganizationalUnit -Name $ChildOU -path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" 
-New-ADUser -Name $User -AccountPassword $AccountPassword -Description "Acirrustech Admin User" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -Path "OU=$ChildOU,OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"  -Enabled:$True
+New-ADUser -Name $User -AccountPassword (ConvertTo-SecureString $AccountPassword -AsPlaintext -Force) -Description "Acirrustech Admin User" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -Path "OU=$ChildOU,OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"  -Enabled $True
 #New-ADUser -Path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" -Name "Test" -AccountPassword (ConvertTo-SecureString "Summer01!" -AsPlaintext -Force) -Description "Acirrustech test user" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -PassThru | Enable-ADAccount
 #New-ADOrganizationalUnit  -Name $OrganizationalUnit -ProtectedFromAccidentalDeletion:$false -path "dc=$Domain,dc=$DomainEnding"
 
-ForEach ($Group in $Groups) {
-    Add-ADGroupMember -Identity $Group -Members $User
-}
+#ForEach ($Group in $Groups) {
+#   Add-ADGroupMember -Identity $Group -Members $User
+#}
 
-Add-ADPrincipalGroupMembership $User -MemberOf $Group
+#Add-ADPrincipalGroupMembership $User -MemberOf $Group
 
-#Add-ADGroupMember 'Domain Admins' Admin
+Add-ADGroupMember 'Domain Admins' $User
 
 
 #Restart-Computer -Force
