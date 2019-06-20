43a0a0171b7509e57a4da351f60f9a856100ad10
florinen <nenfl24@gmail.com>
2019-06-20 10:02:34 -0500

    added new OU and user

--- ad/ad_add_domain_users.ps1
+++ ad/ad_add_domain_users.ps1
@@ -1,4 +1,5 @@
 $OrganizationalUnit = "Acirrustech"
+$ChildOU = "Users"
 $Domain = "acirrustech"
 $DomainEnding = "com"
 $City = "Chicago"
@@ -6,8 +7,9 @@ $City = "Chicago"
 #Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'AD_add_users' -Value "c:\windows\system32\cmd.exe /c C:\scripts\ad_add_domain_users.bat"
 
 Import-Module ActiveDirectory
-New-ADOrganizationalUnit -Name $OrganizationalUnit -City $City -path "dc=$Domain,dc=$DomainEnding" 
-New-ADUser -Name "Admin" -AccountPassword (ConvertTo-SecureString "test123-Admin!!!8" -AsPlaintext -Force) -Description "Acirrustech Setup Account" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -Path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"  -Enabled:$True
+New-ADOrganizationalUnit -Name $OrganizationalUnit -City $City -path "dc=$Domain,dc=$DomainEnding"
+New-ADOrganizationalUnit -Name $ChildOU -path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" 
+New-ADUser -Name "Admin" -AccountPassword (ConvertTo-SecureString "test123-Admin!!!8" -AsPlaintext -Force) -Description "Acirrustech Admin User" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -Path "OU=$ChildOU,OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"  -Enabled:$True
 #New-ADUser -Path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" -Name "Test" -AccountPassword (ConvertTo-SecureString "Summer01!" -AsPlaintext -Force) -Description "Acirrustech test user" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -PassThru | Enable-ADAccount
 #New-ADOrganizationalUnit  -Name $OrganizationalUnit -ProtectedFromAccidentalDeletion:$false -path "dc=$Domain,dc=$DomainEnding"
 
