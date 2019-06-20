88fe66740eec98c5c5c04171086dc38bdaee7be0
florinen <nenfl24@gmail.com>
2019-06-20 15:52:28 -0500

    removed some oprions

--- ad/ad_add_domain_users.ps1
+++ ad/ad_add_domain_users.ps1
@@ -13,7 +13,7 @@ $AccountPassword = "Summer-01!!!*"
 Import-Module ActiveDirectory
 New-ADOrganizationalUnit -Name $OrganizationalUnit -City $City -path "dc=$Domain,dc=$DomainEnding"
 New-ADOrganizationalUnit -Name $ChildOU -path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" 
-New-ADUser -Name $User -AccountPassword (ConvertTo-SecureString $AccountPassword -AsPlaintext -Force) -Description "Acirrustech Admin User" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -Path "OU=$ChildOU,OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"  -Enabled $True
+New-ADUser -Name $User -AccountPassword (ConvertTo-SecureString $AccountPassword -AsPlaintext -Force) -PasswordNeverExpires:$True -Path "OU=$ChildOU,OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"  -Enabled $True
 #New-ADUser -Path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" -Name "Test" -AccountPassword (ConvertTo-SecureString "Summer01!" -AsPlaintext -Force) -Description "Acirrustech test user" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -PassThru | Enable-ADAccount
 #New-ADOrganizationalUnit  -Name $OrganizationalUnit -ProtectedFromAccidentalDeletion:$false -path "dc=$Domain,dc=$DomainEnding"
 
