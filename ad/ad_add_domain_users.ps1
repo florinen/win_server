4db33f368545730634fa2f589133d38511311d4b
florinen <nenfl24@gmail.com>
2019-06-20 12:08:00 -0500

    updated groups

--- ad/ad_add_domain_users.ps1
+++ ad/ad_add_domain_users.ps1
@@ -3,8 +3,9 @@ $ChildOU = "Users"
 $Domain = "acirrustech"
 $DomainEnding = "com"
 $City = "Chicago"
-$groups = 'Domain Admins','Domain Users','Administrators','Enterprise Admins','Group Policy Creator Owners','Schema Admins'
-$user = "Admin"
+$Group = "Administrators"
+$Groups = 'Domain Admins','Domain Users','Administrators','Enterprise Admins','Group Policy Creator Owners','Schema Admins'
+$User = "Admin"
 $AccountPassword = "Summer-01!!!*"
 # Configures script to run once on next logon
 #Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'AD_add_users' -Value "c:\windows\system32\cmd.exe /c C:\scripts\ad_add_domain_users.bat"
@@ -12,14 +13,14 @@ $AccountPassword = "Summer-01!!!*"
 Import-Module ActiveDirectory
 New-ADOrganizationalUnit -Name $OrganizationalUnit -City $City -path "dc=$Domain,dc=$DomainEnding"
 New-ADOrganizationalUnit -Name $ChildOU -path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" 
-New-ADUser -Name $user Set-ADAccountPassword -Identity $user -AccountPassword $AccountPassword -Description "Acirrustech Admin User" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -Path "OU=$ChildOU,OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"  -Enabled:$True
+New-ADUser -Name $User -AccountPassword $AccountPassword -Description "Acirrustech Admin User" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -Path "OU=$ChildOU,OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"  -Enabled:$True
 #New-ADUser -Path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" -Name "Test" -AccountPassword (ConvertTo-SecureString "Summer01!" -AsPlaintext -Force) -Description "Acirrustech test user" -ChangePasswordAtLogon:$False -CannotChangePassword:$True -PasswordNeverExpires:$True -PassThru | Enable-ADAccount
 #New-ADOrganizationalUnit  -Name $OrganizationalUnit -ProtectedFromAccidentalDeletion:$false -path "dc=$Domain,dc=$DomainEnding"
 
-foreach($group in $groups){
-    Add-ADGroupMember -Identity $group -Members "Admin"
+ForEach ($Group in $Groups){
+    Add-ADGroupMember -Identity $Group -Members "Admin"
 }
-
+#Add-ADPrincipalGroupMembership $User -MemberOf $Group
 
 #Add-ADGroupMember 'Domain Admins' Admin
 
