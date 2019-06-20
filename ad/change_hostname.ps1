dfe9a07bec9f4216ffd7d289b8a06723bbb942c9
florinen <nenfl24@gmail.com>
2019-06-20 12:15:59 -0500

    disabled domain var

--- ad/change_hostname.ps1
+++ ad/change_hostname.ps1
@@ -1,5 +1,5 @@
 $NewComputerName = "win-dc01"
-$Domain = "acirrustech"
+#$Domain = "acirrustech"
 # Configures script to run once on next logon
 #Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'AD_Create' -Value "c:\windows\system32\cmd.exe /c C:\scripts\change_hostname.bat"
 
