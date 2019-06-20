c30c0c5a63493b30c2301297701ffff4335b2184
florinen <nenfl24@gmail.com>
2019-06-20 11:43:51 -0500

    enabled rename and restart

--- ad/change_hostname.ps1
+++ ad/change_hostname.ps1
@@ -3,8 +3,8 @@ $Domain = "acirrustech"
 # Configures script to run once on next logon
 #Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'AD_Create' -Value "c:\windows\system32\cmd.exe /c C:\scripts\change_hostname.bat"
 
-#Rename-Computer -NewName $NewComputerName
-#Start-Sleep -Seconds 5
+Rename-Computer -NewName $NewComputerName
+Start-Sleep -Seconds 5
 
-#Restart-Computer -Force
-#update
+Restart-Computer -Force
+update
