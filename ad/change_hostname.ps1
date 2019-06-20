a94cdc3bc5702681733a6f84831294670f991801
florinen <nenfl24@gmail.com>
2019-06-20 15:53:58 -0500

    reconfigured

--- ad/change_hostname.ps1
+++ ad/change_hostname.ps1
@@ -1,10 +1,10 @@
 $NewComputerName = "win-dc01"
-#$Domain = "acirrustech"
-# Configures script to run once on next logon
-#Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'Change_hostname' -Value "c:\windows\system32\cmd.exe /c C:\scripts\change_hostname.bat"
-
 Rename-Computer -NewName $NewComputerName
 Start-Sleep -Seconds 5
 
 Restart-Computer -Force
 update
+
+#$Domain = "acirrustech"
+# Configures script to run once on next logon
+#Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'Change_hostname' -Value "c:\windows\system32\cmd.exe /c C:\scripts\change_hostname.bat"
