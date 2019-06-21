59962c088d6443454b62300ec7a591b3f74f7a3d
florinen <nenfl24@gmail.com>
2019-06-20 21:53:58 -0500

    removed on line

--- ad/ad_init.ps1
+++ ad/ad_init.ps1
@@ -12,7 +12,7 @@ $SysvolPath = "C:\Windows\SYSVOL"
 
 
 ## Configures script to run once on next logon
-#Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'AD_Autologon' -Value "c:\windows\system32\cmd.exe /c C:\scripts\ad_init.bat"
+
 Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'AD_Create_users' -Value "c:\windows\system32\cmd.exe /c C:\scripts\ad_add_domain_users.bat"
 Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'Set-AutoLogon' -Value "c:\windows\system32\cmd.exe /c C:\scripts\auto_logon.bat"
 Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'Change_hostname' -Value "c:\windows\system32\cmd.exe /c C:\scripts\change_hostname.bat"
