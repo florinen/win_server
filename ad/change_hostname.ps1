61bd340e0b548d3cee9b00c84a432fdd72e7c139
florinen <nenfl24@gmail.com>
2019-06-20 21:55:41 -0500

    cleaned up

--- ad/change_hostname.ps1
+++ ad/change_hostname.ps1
@@ -5,6 +5,4 @@ Start-Sleep -Seconds 5
 Restart-Computer -Force
 update
 
-#$Domain = "acirrustech"
-# Configures script to run once on next logon
-#Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name 'Change_hostname' -Value "c:\windows\system32\cmd.exe /c C:\scripts\change_hostname.bat"
+ 
