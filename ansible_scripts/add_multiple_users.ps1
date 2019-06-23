ee75473e6359aea7e8ea3a6a4f00e8f69a7e2331
florinen <nenfl24@gmail.com>
2019-06-23 10:46:41 -0500

    updated files

--- ansible_scripts/add_multiple_users.ps1
+++ ansible_scripts/add_multiple_users.ps1
@@ -1,13 +1,14 @@
+Import-Module activedirectory
 $Users = Import-Csv -Path "C:\scripts\ansible_scripts\add_multiple_users.csv"            
 foreach ($User in $Users)            
 {            
-    $Displayname = $User.Firstname + " " + $User.Lastname            
-    $UserFirstname = $User.Firstname            
-    $UserLastname = $User.Lastname            
+    $Displayname = $User.FirstName + " " + $User.LastName            
+    $UserFirstname = $User.FirstName            
+    $UserLastname = $User.LastName            
     $OU = "$User.OU"            
-    $SAM = $User.SAM            
-    $UPN = $User.Firstname + "." + $User.Lastname + "@" + $User.Maildomain            
+    $SAM = $User.SamAccountName            
+    $UPN = $User.FirstName + "." + $User.LastName + "@" + $User.Email            
     $Description = $User.Description            
     $Password = $User.Password            
-    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server domain.loc            
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server  win-dc01.devopnet.com        
 }
