c0b0764f956c9874f0351f977a0fcdedf4bf6caf
florinen <nenfl24@gmail.com>
2019-07-04 12:13:36 -0500

    restructure AD

--- ad/ad_init.ps1
+++ ad/ad_init.ps1
@@ -36,8 +36,13 @@ Set-ItemProperty $RegPath "AutoLogonCount" -Value "3" -type DWord
 Write-Host "Windows Server 2016 - Active Directory Installation"
 
 Write-Host " - Installing AD-Domain-Services..."
-Install-WindowsFeature -Name GPMC,RSAT-AD-PowerShell,RSAT-AD-AdminCenter,RSAT-ADDS-Tools,RSAT-DNS-Server
-Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
+Install-WindowsFeature -Name GPMC,RSAT-AD-PowerShell
+                       -RSAT-AD-AdminCenter
+                       -RSAT-ADDS-Tools
+                       -RSAT-DNS-Server
+                       
+Install-windowsfeature  -name AD-Domain-Services 
+                        -IncludeManagementTools
 
 Import-Module ADDSDeployment
 
