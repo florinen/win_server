234e6dfc361e21fb725995a7d4969f2f1469842b
florinen <nenfl24@gmail.com>
2019-07-04 11:46:46 -0500

    updated

--- ad/ad_add_domain_users.ps1
+++ ad/ad_add_domain_users.ps1
@@ -13,14 +13,14 @@ $OU = "OU=$ChildOU,OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding"
 Import-Module ActiveDirectory
 New-ADOrganizationalUnit -Name $OrganizationalUnit -City $City -path "DC=$Domain,DC=$DomainEnding"
 New-ADOrganizationalUnit -Name $ChildOU -path "OU=$OrganizationalUnit,DC=$Domain,DC=$DomainEnding" 
-New-ADUser  -Name $User `
-            -AccountPassword (ConvertTo-SecureString $AccountPassword -AsPlaintext -Force) `
-            -PasswordNeverExpires:$True `
-            -Path $OU `
-            -Description "Acirrustech test user" `
-            -ChangePasswordAtLogon:$False `
-            -CannotChangePassword:$True `
-            -PasswordNeverExpires:$True `
+New-ADUser  -Name $User 
+            -AccountPassword (ConvertTo-SecureString $AccountPassword -AsPlaintext -Force) 
+            -PasswordNeverExpires:$True 
+            -Path $OU 
+            -Description "Acirrustech test user" 
+            -ChangePasswordAtLogon:$False 
+            -CannotChangePassword:$True 
+            -PasswordNeverExpires:$True 
             -Enabled $True 
 
 
